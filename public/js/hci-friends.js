3efa8b732f2ff0cd4c8968c6479c28a3394044a6
vagrant <vagrant@precise32.(none)>
2014-02-06 00:18:22 +0000

    putting it together lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -3,6 +3,13 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+
+	$('.friendName').click(function(e){
+			e.preventDefault();
+			var name = $(this).text();
+			var newName = anagrammedName(name);
+			$(this).text(newName);
+	});
 })
 
 /*
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,14 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
+exports.addFriend = function(req, res) {  
+	var name = req.query.name;
+	var description = req.query.description;
+	var newFriend = {
+		'imageURL' : 'http://lorempixel.com/400/400/people',
+		'name' : name,
+		'description': description
+	}
+
+	data['friends'].push(newFriend);
+	res.render('add');
  }
